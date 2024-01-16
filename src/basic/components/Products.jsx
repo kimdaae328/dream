cda78a7f4714bf1df38c94a02aedc531fbd45bde
kimdaae <kimdaae328@gmail.com>
2024-01-16 21:30:51 +0900

    추가.

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import useProducts from '../../hooks/use-products';
 
-export default function Products() {    
+export default function Products() {        
   const [checked, setChecked] = useState(false);       
   const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked((prev) => !prev);
